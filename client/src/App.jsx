f044bba836a68b5c85b4ae94455cd2db8c0114eb
Bibek Ghimire <bibekghimire773@gmail.com>
2026-06-17 18:39:27 +0545

    feat:created connection between  Application api and react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,1 +1,18 @@
-// Root App component with routing - to be implemented
+import { useEffect } from "react";
+import { getApplications } from "./services/applicationService";
+
+function App() {
+    useEffect(() => {
+        getApplications()
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
+
+    return <h1>Job Application Tracker</h1>;
+}
+
+export default App;
--- client/src/services/applicationService.js
+++ client/src/services/applicationService.js
@@ -1,1 +1,8 @@
-// API calls for applications using axios - to be implemented
+import axios from "axios";
+
+const API_URL = "http://localhost:5000/api/applications";
+
+export const getApplications = async () => {
+    const response = await axios.get(API_URL);
+    return response.data;
+};
