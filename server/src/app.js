d9fb134005fdbdc5be600564d07e7c2c79c1535d
Bibek Ghimire <bibekghimire773@gmail.com>
2026-06-17 18:08:25 +0545

    fix: Renamed route job to applications

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,11 @@
 // app.js
 import express from "express";
 import applicationRoutes from "./routes/applicationRoutes.js";
+import cors from "cors";
 
 const app = express();
-
+app.use(cors())
 app.use(express.json());
-app.use("/api/job", applicationRoutes);
+app.use("/api/applications", applicationRoutes);
 
 export default app;
